dcf5419f9e1a34555be6d63d8e925b4db17eff34
unknown <svyat.teterin@gmail.com>
2022-03-26 01:39:05 +0300

    Изменил скролл бар, добавил прелодер

---
+++ src/modules/prepoader.js
@@ -0,0 +1,12 @@
+const prepoader = () => {
+
+    window.onload = function () {
+        document.body.classList.add('loaded_hiding');
+        window.setTimeout(function () {
+          document.body.classList.add('loaded');
+          document.body.classList.remove('loaded_hiding');
+        }, 500);
+      }
+}
+
+export default prepoader
