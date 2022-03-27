be4fd2e0c4ebe02fc3adc12636229146e35b710b
unknown <svyat.teterin@gmail.com>
2022-03-27 21:26:07 +0300

    Добавил страницу contacts и валидацию телефона

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import header from './modules/header'
 import promo from './modules/promo'
 import aboutSlider from './modules/aboutSlider'
 import prepoader from './modules/prepoader'
+import phoneValid from './modules/phoneValid'
 
 sliders()
 header()
 promo()
 aboutSlider()
 prepoader()
+phoneValid()
+
