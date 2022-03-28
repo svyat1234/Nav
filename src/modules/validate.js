6824299b752c0939abe5f1040d1d39fe95ddfbda
unknown <svyat.teterin@gmail.com>
2022-03-29 00:42:02 +0300

    Отправка данных методом fetch

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -5,6 +5,7 @@ const validate = () => {
     const inputs = document.querySelectorAll('input')
     const errorName = document.querySelector('.form-error-name')
     const errorTel = document.querySelector('.form-error-tel')
+    const condition = document.querySelector('.condition')
     // const input = document.querySelectorAll('input')
 
     let name = /[^a-zа-яё\s]/gi
@@ -12,27 +13,61 @@ const validate = () => {
         this.value = this.value.replace(name, '');
     }
 
+    let nameValid = false
+    let telValid = false
+
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         if (inputName.value.length === 0) {
             inputName.style.border = "1px solid tomato"
             errorName.textContent = "Заполните поле"
             errorName.style.display = "block"
+            nameValid = false
         } else if (inputName.value.length < 2) {
             inputName.style.border = "1px solid tomato"
             errorName.textContent = "Введите больше одного символа"
             errorName.style.display = "block"
+            nameValid = false
+        } else {
+            nameValid = true
         }
         if(inputTel.value.length === 0) {
             inputTel.style.border = "1px solid tomato"
             errorTel.textContent = "Заполните поле"
             errorTel.style.display = "block"
+            telValid = false
         } else if (inputTel.value.length < 18) {
             inputTel.style.border = "1px solid tomato"
             errorTel.textContent = "Введите 10 символов"
             errorTel.style.display = "block"
+            telValid = false
+        } else {
+            telValid = true
         }
-        console.log(inputTel.value.length);
+        if(nameValid && telValid) {
+            condition.textContent = "Отправка..."
+            console.log("Ура!");
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: inputName.value,
+                    tel: inputTel.value
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                inputName.value = ''
+                inputTel.value = ''
+                condition.textContent = "Отправлено!"
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
     })
     inputs.forEach(input => {
         input.addEventListener('keydown', () => {
